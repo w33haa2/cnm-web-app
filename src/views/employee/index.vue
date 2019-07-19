938583f3a5ec23ddb2142c5604334dc61a843e1e
Emmanuel James <jamesenglajom@gmail.com>
2019-07-19 20:11:22 +0800

    Emman > update dialog for employee,dialog for sanction type and level

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container">
+    <h4>Employee</h4>
     <div class="filter-container">
       <!-- ADD EMPLOYEE BUTTON & EXCEL EXPORT/IMPORT BUTTTONS -->
       <el-row :gutter="8" style="padding-right:8px;margin-bottom:30px;">
@@ -10,40 +11,28 @@
           :lg="{span: 12}"
           :xl="{span: 12}"
         >
-          <el-button size="mini" @click="form.toggle = true">Add Employee</el-button>
+          <el-button
+            size="mini"
+            @click="form.toggle = true, form.action_data={action:'Create',data:null}"
+          >Create Employee</el-button>
           <el-button-group>
-            <el-button size="mini">Import</el-button>
+            <el-button size="mini" @click="$refs.excelInput.click()">Import</el-button>
+            <input
+              ref="excelInput"
+              type="file"
+              @change="excelChanged"
+              style="display:none"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            />
             <el-button size="mini">Export</el-button>
           </el-button-group>
         </el-col>
       </el-row>
-
-      <!-- EMPLOYEE FORM ELEMENT -->
-      <!-- <el-row v-if="form.toggle" :gutter="8" style="padding-right:8px;margin-bottom:30px;">
-        <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 24}" :xl="{span:24}">
-          <employee-form :toggle="form.toggle" :data />
-        </el-col>
-      </el-row>-->
       <employee-form
         :toggle="form.toggle"
-        :data="{action:'Add', id:null}"
+        :data="form.action_data"
         @closeEmployeeModal="closeEmployeeModal"
       />
-
-      <!-- MULTI SEARCH & FILTER, CHANGE STATUS ELEMENTS -->
-      <!-- <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 20}" :xl="{span:20}" style="padding-right:8px;margin-bottom:10px;">
-        <select-search style="width:100%" :multiple="true" :filterable="true" :remote="true" :size="'mini'" autocomplete="on" placeholder="Search and select..." :data="select_search[filtered.by].data" @filter="filterEmployee" />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 4}" :xl="{span:4}" style="padding-right:8px;margin-bottom:10px;">
-        <el-button size="mini" type="danger" style="width:100%" :disabled="multiSelect.length===0">Change Status</el-button>
-        <el-select v-model="filtered.by" style="width:100%" size="mini">
-          <el-option v-for="option in options.filter" :key="option.value" :value="option.value" :label="option.label" />
-        </el-select>
-      </el-col>
-      </el-row>-->
-
-      <!-- SIMPLE BACKEND SEARCH INPUT -->
       <!-- DISPLAY RECORDS & PAGINATION -->
       <el-row :gutter="8" style="padding-right:8px;margin-bottom:5px;">
         <el-col
@@ -94,20 +83,6 @@
         </el-col>
       </el-row>
 
-      <!-- <el-row :gutter="2">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 24}" :xl="{span: 24}" style="padding-right:8px;margin-bottom:10px;">
-
-        <el-tag>
-          {{ filtered.count+' record/s' }}
-        </el-tag>
-        <el-tag>
-          {{ filtered.count+' displayed' }}
-        </el-tag>
-        <el-tag>
-          {{ filtered.count+' selected' }}
-        </el-tag>
-      </el-col>
-      </el-row>-->
       <el-row :gutter="8" style="padding-right:8px;margin-bottom:30px;">
         <el-col
           :xs="{span: 24}"
@@ -116,28 +91,22 @@
           :lg="{span: 24}"
           :xl="{span: 24}"
         >
-          <transaction-table :table-data="employeesData" />
+          <transaction-table :table-data="employeesData" @dropdownCommand="dropdownCommand" />
         </el-col>
-        <!-- <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <todo-list />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <box-card />
-        </el-col>-->
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
-import TransactionTable from './components/TransactionTable'
-import SelectSearch from './components/select_search'
-import EmployeeForm from './components/EmployeeForm'
-import { mapGetters, mapActions } from 'vuex'
-import { Message } from 'element-ui'
-
+import TransactionTable from "./components/TransactionTable";
+import SelectSearch from "./components/select_search";
+import EmployeeForm from "./components/EmployeeForm";
+import { mapGetters, mapActions } from "vuex";
+import { Message } from "element-ui";
+import excel from "xlsx";
 export default {
-  name: 'DashboardHR',
+  name: "DashboardHR",
   components: {
     TransactionTable,
     SelectSearch,
@@ -145,20 +114,20 @@ export default {
   },
   data() {
     return {
-      searchQuery: '',
+      searchQuery: "",
       table_config: {
         searchable_fields: [
-          { value: 'full_name', label: 'Name' },
-          { value: 'position', label: 'Position' },
-          { value: 'email', label: 'Email' },
-          { value: 'contract', label: 'Contract' },
-          { value: 'status', label: 'Status' },
-          { value: 'gender', label: 'Gender' },
-          { value: 'address', label: 'Address' }
+          { value: "full_name", label: "Name" },
+          { value: "position", label: "Position" },
+          { value: "email", label: "Email" },
+          { value: "contract", label: "Contract" },
+          { value: "status", label: "Status" },
+          { value: "gender", label: "Gender" },
+          { value: "address", label: "Address" }
         ],
         query: {
           search: {
-            target: 'full_name',
+            target: "full_name",
             query: null
           },
           limit: 10,
@@ -175,81 +144,110 @@ export default {
       },
       form: {
         toggle: false,
-        action: {
-          value: 'add',
-          label: 'Add'
+        action_data: {
+          action: "Create",
+          data: null
         }
       }
-    }
+    };
   },
   mounted() {
     // fetch and commit table data via vuex action
-    const data = this.query
-    this.fetchEmployees({ data })
-    this.$root.$on('employee_table.refresh', this.refreshTable)
+    const data = this.query;
+    this.fetchEmployees({ data });
+    this.$root.$on("employee_table.refresh", this.refreshTable);
     // setup filter select
   },
   computed: {
     ...mapGetters([
-      'employees',
-      'allPosition',
-      'employeesData',
-      'employeesTotal',
-      'employeesFetchState'
+      "employees",
+      "allPosition",
+      "employeesData",
+      "employeesTotal",
+      "employeesFetchState"
     ])
   },
   watch: {
     employeeData: function(v) {
-      console.log(v)
+      console.log(v);
     },
     searchQuery: function(newData) {
-      if (newData !== '') {
-        this.query['target[]'] = this.table_config.query.search.target
-        this.query.query = newData
+      if (newData !== "") {
+        this.query["target[]"] = this.table_config.query.search.target;
+        this.query.query = newData;
         // this.query.target = "full_name"
         // this.query.query = newData
-        const data = this.query
-        this.fetchEmployees({ data })
+        const data = this.query;
+        this.fetchEmployees({ data });
       } else {
-        this.query['target[]'] = ''
-        this.query.query = ''
-        const data = this.query
-        this.fetchEmployees({ data })
+        this.query["target[]"] = "";
+        this.query.query = "";
+        const data = this.query;
+        this.fetchEmployees({ data });
       }
     },
     employeesFetchState({ initial, success, fail }) {
       if (fail) {
-        Message.error({ message: this.irErrors, duration: '2500' })
+        Message.error({ message: this.irErrors, duration: "2500" });
       }
     }
   },
   methods: {
-    ...mapActions(['fetchUsers', 'fetchPositions', 'fetchEmployees']),
+    ...mapActions(["fetchUsers", "fetchPositions", "fetchEmployees"]),
+    dropdownCommand(v) {
+      switch (v.action) {
+        case "edit":
+          this.form.toggle = true;
+          this.form.action_data = {
+            action: "Update",
+            data: this.getUpdateData(v.id)
+          };
+          break;
+      }
+    },
+    getUpdateData(id) {
+      console.log(this.employeesData.filter(i => i.id == id)[0]);
+
+      return this.employeesData.filter(i => i.id == id)[0];
+    },
+    excelChanged(e) {
+      var files = e.target.files,
+        f = files[0];
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var data = new Uint8Array(e.target.result);
+        var workbook = excel.read(data, { type: "array" });
+
+        /* DO SOMETHING WITH workbook HERE */
+      };
+      reader.readAsArrayBuffer(f);
+      console.log(reader);
+    },
     closeEmployeeModal(v) {
-      this.form.toggle = v
+      this.form.toggle = v;
     },
     refreshTable() {
-      const data = this.query
-      this.fetchEmployees({ data })
+      const data = this.query;
+      this.fetchEmployees({ data });
     },
     tableSizeChange(value) {
-      this.query.limit = value
-      const data = this.query
-      this.fetchEmployees({ data })
+      this.query.limit = value;
+      const data = this.query;
+      this.fetchEmployees({ data });
     },
     tablePageChange(value) {
-      this.query.offset = (value - 1) * this.query.limit
-      const data = this.query
-      this.fetchEmployees({ data })
+      this.query.offset = (value - 1) * this.query.limit;
+      const data = this.query;
+      this.fetchEmployees({ data });
     },
     onColumnSort(value) {
-      this.query.sort = value.sort
-      this.query.order = value.order ? 'asc' : 'desc'
-      const data = this.query
-      this.fetchEmployees({ data })
+      this.query.sort = value.sort;
+      this.query.order = value.order ? "asc" : "desc";
+      const data = this.query;
+      this.fetchEmployees({ data });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
