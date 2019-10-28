7906b0f593db4ea5f67b8229e580c3a92f4ca33a
Emmanuel James <jamesenglajom@gmail.com>
2019-10-28 21:20:16 +0800

    Emman > updated subordinates tab on profile page, with search&pagination

--- src/views/profile/components/Subordinates.vue
+++ src/views/profile/components/Subordinates.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="app-container">
-    <h4 style="color:#646464">Subordinates</h4>
-
-    <!-- Search and Pagination -->
-    <el-row :gutter="10">
-      <el-col :md="{ span:4 }">
-          <el-date-picker
-            v-model="table_config.query.date"
-            size="mini"
-            type="date"
-            format="yyyy-MM-dd"
-            value-format="yyyy-MM-dd"
-            :clearable="false"
-            style="width:100%"
-          />
-      </el-col>
-      <el-col :md="{ span:5 }">
+    <el-row :gutter="10" style="margin-bottom:5px;">
+      <el-col :md="{ span:4}">
         <el-select
           v-model="table_config.remoteFilter.select"
           class="form-input"
@@ -31,109 +17,148 @@
           <el-option
             v-for="(option,index) in table_config.remoteFilter.options"
             :key="index"
-            :label="option[table_config.remoteFilter.by].full_name"
-            :value="option[table_config.remoteFilter.by].id"
+            :label="option.child_details.full_name"
+            :value="option.child_details.id"
           />
         </el-select>
       </el-col>
+      <el-col :md="{ span:12, offset:8 }">
+        <el-pagination
+          style="float:right"
+          :pager-count="4"
+          :page-sizes="[10,25,50]"
+          :page-size="table_config.query.perpage"
+          layout="total, sizes, prev, pager, next"
+          :total="table_config.count"
+          :current-page.sync="table_config.query.page"
+          background
+          small
+          @current-change="tablePageChange"
+          @size-change="tableSizeChange"
+        />
+      </el-col>
     </el-row>
-
     <!-- Table -->
-    <el-table :data="table_config.data"  style="width: 100%;margin-top:30px;" v-loading="fetchHierarchyLogsState.initial">
-      <el-table-column align="center" label="Subordinate">
-        <template slot-scope="scope">{{ scope.row.child_details.full_name }}</template>
-      </el-table-column>
-      <el-table-column align="center" label="Start date">
-        <template slot-scope="scope">{{ formatDate(scope.row.start_date,"","MM Do YYYY") }}</template>
-      </el-table-column>
-      <el-table-column align="center" label="End date">
+    <el-table
+      :data="table_config.data"
+      style="width: 100%;margin-top:30px;"
+      v-loading="fetchHierarchyLogsState.initial"
+    >
+      <el-table-column label="Name">
         <template slot-scope="scope">
-          <template v-if="scope.row.end_date">
-            {{ formatDate(scope.row.end_date,"","MM Do YYYY") }}
-          </template>
-          <template v-else>
-            <el-tag type="success" size="mini">At present</el-tag>
-          </template>
+          <router-link :to="'/profile/index/'+scope.row.id">{{ scope.row.child_details.full_name }}</router-link>
         </template>
+      </el-table-column>
+      <el-table-column label="Email">
+        <template slot-scope="scope">{{ scope.row.child_user_details.email }}</template>
+      </el-table-column>
+      <el-table-column label="Position">
+        <template slot-scope="scope">{{ scope.row.child_user_details.access.name }}</template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import moment from "moment";
 import { mapActions, mapGetters } from "vuex";
-const ymdHms ="YYYY-MM-DD HH:mm:ss"
+
+const ymdHms = "YYYY-MM-DD HH:mm:ss";
+
 export default {
   data() {
     return {
-      table_config:{
-        data:[],
-        dummy:[],
-        count:0,
-        loader:false,
-        page:1,
-        query:{
-          date:moment().format(ymdHms),
-          parent_id: this.$route.params.id
+      table_config: {
+        data: [],
+        dummy: [],
+        count: 0,
+        loader: false,
+        query: {
+          date: moment().format(ymdHms),
+          parent_id: this.$route.params.id,
+          page: 1,
+          perpage: 10
         },
-        remoteFilter:{
-          select:null,
-          options:[],
-          by:"child_details",
-          loader:false,
+        remoteFilter: {
+          select: null,
+          options: [],
+          by: "all",
+          loader: false
         }
       }
-    }
+    };
   },
   computed: {
     ...mapGetters([
-      "fetchHierarchyLogsState","fetchHierarchyLogsData","fetchHierarchyLogsTitle",
-      ]),
+      "token",
+      "fetchHierarchyLogsState",
+      "fetchHierarchyLogsData",
+      "fetchHierarchyLogsTitle"
+    ])
   },
-  watch:{
-    "table_config.query.date":function(v){
+  watch: {
+    "table_config.query.parent_id": function() {
       this.fetchHierarchyLogs(this.table_config.query);
     },
-    "table_config.remoteFilter.select":function(v){
-      if(v){
-        this.table_config.data = this.table_config.dummy.filter(i=> i.child_id == v);
-      }else{
-        this.table_config.data = this.table_config.dummy;
+    "table_config.remoteFilter.select": function(v) {
+      if (v) {
+        this.table_config.query.child_id = v;
+      } else {
+        delete this.table_config.query.child_id;
       }
+      this.fetchHierarchyLogs(this.table_config.query);
     },
-    fetchHierarchyLogsState({initial, success, fail}){
-      if(success){
-        this.table_config.data = this.fetchHierarchyLogsData.hierarchy_log;
-        this.table_config.dummy = this.fetchHierarchyLogsData.hierarchy_log;
+    fetchHierarchyLogsState({ initial, success, fail }) {
+      if (success) {
+        this.table_config.data = this.fetchHierarchyLogsData.hierarchy_log.data;
         this.table_config.count = this.fetchHierarchyLogsData.count;
+        this.table_config.page = this.fetchHierarchyLogsData.hierarchy_log.current_page;
       }
-    },
+    }
   },
   created() {
     this.fetchHierarchyLogs(this.table_config.query);
   },
   methods: {
-    ...mapActions(["fetchHierarchyLogs"]),
-    tableSizeChange(){},
-    tablePageChange(){},
-    remoteSearch(query) {
-      let filtered = null;
-      this.table_config.remoteFilter.options = [];
+    ...mapActions(["fetchHierarchyLogs", "deleteHierarchyLog"]),
+    getTableFilterOptions(url) {
+      let options = {
+        headers: {
+          Authorization: "Bearer " + this.token
+        }
+      };
 
-      this.table_config.remoteFilter.loader = true;
-      if (query !== "") {
-        filtered = this.table_config.dummy.filter(i=> i[this.table_config.remoteFilter.by].full_name.toLowerCase().includes(query));
-      } else if(query !== "all"){
-        filtered = this.table_config.dummy;
-      }
-      this.table_config.remoteFilter.options = filtered;
-      this.table_config.remoteFilter.options.unshift({child_details:{id:null,full_name:""}});
-      this.table_config.remoteFilter.loader = false;
-
+      axios
+        .get(url, options)
+        .then(res => {
+          this.table_config.remoteFilter.options = res.data.meta.hierarchy_log;
+        })
+        .catch(err => console.log(err.response.data));
     },
+    tableSizeChange(v) {
+      this.table_config.query.page = 1;
+      this.table_config.query.perpage = v;
+      this.fetchHierarchyLogs(this.table_config.query);
+    },
+    tablePageChange(v) {
+      this.table_config.query.page = v;
+      this.fetchHierarchyLogs(this.table_config.query);
+    },
+    remoteSearch(query) {
+      let url =
+        "api/v1/hierarchy_log?list=true&date=" +
+        moment(this.table_config.query.date).format(ymdHms) +
+        "&parent_id=" +
+        this.table_config.query.parent_id +
+        "&filter_by=child_details";
+      if (query != "") {
+        url += "&target[]=full_name&query=" + query;
+      }
+      this.getTableFilterOptions(url);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
