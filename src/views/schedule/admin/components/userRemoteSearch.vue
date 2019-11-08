95ac3d56bd82ce6519a110589cf798b11c8fa096
Emmanuel James <jamesenglajom@gmail.com>
2019-11-08 16:29:05 +0800

    Emman > created userRemoteSearch component

---
+++ src/views/schedule/admin/components/userRemoteSearch.vue
@@ -0,0 +1,73 @@
+<template>
+  <div style="width:100%">
+    <el-select
+      size="mini"
+      remote
+      filterable
+      :multiple="multiple"
+      :placeholder="placeholder"
+      v-model="select"
+      :remote-method="remoteMethod"
+      :loading="loading"
+      style="width:100%"
+      :disabled="disabled"
+    >
+      <template v-for="(option,index) in options">
+        <el-option :key="index" :value="option.id" :label="option.full_name" />
+      </template>
+    </el-select>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  props: ["placeholder", "params", "multiple", "disabled", "clear"],
+  data() {
+    return {
+      select: null,
+      options: [],
+      loading: false
+    };
+  },
+  watch: {
+    clear(v) {
+      this.select = null;
+      this.options = [];
+    },
+    select(v) {
+      this.$emit("selected", v);
+    }
+  },
+  computed: {
+    ...mapGetters(["token"])
+  },
+  methods: {
+    remoteMethod(q) {
+      this.loading = true;
+      if (q) {
+        this.params.target = ["full_name"];
+        this.params.query = q;
+      } else {
+        this.params.target = null;
+        this.params.query = null;
+      }
+      let url = "api/v1/users" + this.toUrlParams(this.params),
+        options = {
+          headers: {
+            Authorization: "Bearer " + this.token
+          }
+        };
+
+      this.axiosRequest("get", url, options).then(res => {
+        this.loading = false;
+        if (res.code == 200) {
+          this.options = res.meta.metadata;
+        } else {
+          this.options = [];
+        }
+      });
+    }
+  }
+};
+</script>
