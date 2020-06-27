2b7fa43d2e0cd2c29f96f0b7f5419a2d21596738
Floyd Matabilas <floydignified@gmail.com>
2020-06-27 13:58:59 +0800

    Floyd > fix console error on right sidebar size

--- src/components/right_side_bar/index.vue
+++ src/components/right_side_bar/index.vue
@@ -1,52 +1,53 @@
 <template>
-    <div>
-        <el-drawer 
-        :visible.sync="drawer" 
-        direction="rtl" 
-        :modal-append-to-body="false" 
-        :append-to-body="false"
-        :show-close="false"
-        :withHeader="false"
-        :size="230"
-        custom-class="time-logger-drawer">
-            <time-logger></time-logger>
-            <!-- work history -->
-            <div style="border-left:4px solid gray;background-color:#f0f3f5;border-bottom:#eee 1px solid" class="p-10">
-                <div style="font-weight:800;color:gray;font-size:.75em;text-align:center">WORK HISTORY</div>
-            </div>
-            <!-- contents -->
-            <previous-work :toggle="toggle.previous_work"></previous-work>
-        </el-drawer>
-        <div class="drawer-button-wrapper" :class="drawer?'active':''" :style="drawer?'display:none':''">
-            <div class="drawer-button" @click="drawer=!drawer">
-                <i class="el-icon-timer drawer-button-icon" />
-            </div>
-        </div>
+  <div>
+    <el-drawer
+      :visible.sync="drawer"
+      direction="rtl"
+      :modal-append-to-body="false"
+      :append-to-body="false"
+      :show-close="false"
+      :with-header="false"
+      size="230"
+      custom-class="time-logger-drawer"
+    >
+      <time-logger />
+      <!-- work history -->
+      <div style="border-left:4px solid gray;background-color:#f0f3f5;border-bottom:#eee 1px solid" class="p-10">
+        <div style="font-weight:800;color:gray;font-size:.75em;text-align:center">WORK HISTORY</div>
+      </div>
+      <!-- contents -->
+      <previous-work :toggle="toggle.previous_work" />
+    </el-drawer>
+    <div class="drawer-button-wrapper" :class="drawer?'active':''" :style="drawer?'display:none':''">
+      <div class="drawer-button" @click="drawer=!drawer">
+        <i class="el-icon-timer drawer-button-icon" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import timeLogger from "@/components/time_logger";
-import previousWork from "@/components/previous_work";
+import timeLogger from '@/components/time_logger'
+import previousWork from '@/components/previous_work'
 export default {
-    name:"RightSideBar",
-    components:{timeLogger,previousWork},
-    data(){
-        return {
-            drawer:true,
-            toggle:{
-                previous_work:true,
-            },
-            refresh:false
-        }
-    },
-    watch:{
-        drawer(v){
-            if(v){
-                this.refresh = !this.refresh;
-            }
-        }
+  name: 'RightSideBar',
+  components: { timeLogger, previousWork },
+  data() {
+    return {
+      drawer: true,
+      toggle: {
+        previous_work: true
+      },
+      refresh: false
     }
+  },
+  watch: {
+    drawer(v) {
+      if (v) {
+        this.refresh = !this.refresh
+      }
+    }
+  }
 }
 </script>
 
