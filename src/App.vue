cacc9fddcdb07c195284f8bdcaa3985c26aa6efd
Emman <jamesenglajom@gmail.com>
2020-07-21 21:34:19 +0800

    Emman > added condition for timelogger component -> agent only

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <time-logger v-if="$route.path!='/login'"></time-logger>
+    <time-logger v-if="$route.path!='/login' && position=='Representative - Order Placer'"></time-logger>
     <router-view />
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex"
 import timeLogger from "@/components/right_side_bar";
 export default {
   components:{timeLogger},
-  name: 'App'
+  name: 'App',
+  computed:{
+    ...mapGetters(["position"])
+  }
 }
 </script>
